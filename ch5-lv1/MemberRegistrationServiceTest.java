73342f6dd01bca544169eb3440b4ef2ca4b3e0b4
Watanabe-Ryo-to <watanabe.ryoto.20260701@gmail.com>
2026-08-15 11:49:31 +0900

    除外リスト更新

--- ch5-lv1/MemberRegistrationServiceTest.java
+++ ch5-lv1/MemberRegistrationServiceTest.java
@@ -1,13 +1,14 @@
 //package com.example.member.service;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class MemberRegistrationServiceTest 
